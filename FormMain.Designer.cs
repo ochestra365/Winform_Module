efc848dfa142eca5e6c46fea7404250b8e1876bb
Park seng chul <ochestra365@naver.com>
2021-08-06 13:58:46 +0900

    Initail Commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace TimerDigitalWatch
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("굴림", 40F);
+            this.lblTime.Location = new System.Drawing.Point(112, 75);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(174, 54);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(323, 215);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(84, 71);
+            this.BtnStop.TabIndex = 1;
+            this.BtnStop.Text = "정지";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(233, 215);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(84, 71);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "시작";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 329);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.lblTime);
+            this.Name = "FormMain";
+            this.Text = "디지털시계";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Button BtnStart;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerDigitalWatch
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            lblTime.Text = "";
+            timer1.Interval = 1000;//1초
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Location = new Point(ClientSize.Width / 2 - lblTime.Width / 2, ClientSize.Height / 2 - lblTime.Height / 2);
+            lblTime.Font = new Font("맑은 고딕", 30, FontStyle.Bold);
+            lblTime.Text = DateTime.Now.ToString();
+            timer1.Tick += timer1_Tick;
+
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
